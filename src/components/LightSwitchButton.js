286b56602d678bdc6e14f7fd215da9c8a91a27a8
bhalemegha <megha.bhale6@gmail.com>
2021-12-06 15:22:16 -0600

    parent state modifications

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
-function LightSwitchButton() {
-  const [light, setLight] = useState('off');
-  const handleClick = () => setLight(light === "on" ? "off" : "on");
+function LightSwitchButton(props) {
+  // const [light, setLight] = useState('off');
+  const {light, switchLight} = props;
+
   return (
-    <button className="LightSwitchButton" onClick={handleClick}>
+    <button className="LightSwitchButton" onClick={switchLight}>
       {light === "on" && <span><i>💡</i> I'm on!</span>}
       {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
